b7d9f7b5108c626c31a24b7d6b7d830a356e1b30
BARIŞ YILMAZ <barisyilmaz@tarimorman.gov.tr>
2025-08-29 15:41:41 +0300

    UPDATED

--- src/UdemyClone.Api/DTOs/RegisterRequest.cs
+++ src/UdemyClone.Api/DTOs/RegisterRequest.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
 namespace UdemyClone.Api.DTOs;
 
 public class RegisterRequest
 {
     public string Email { get; set; } = string.Empty;
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
     public string Password { get; set; } = string.Empty;
     public string? Role { get; set; } // Student | Instructor | Admin
 }
